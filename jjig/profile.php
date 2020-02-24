68ef3796db2bca19a01324c9d9ac236d5366ec3c
georgette-a <45202432+georgette-a@users.noreply.github.com>
2020-02-24 23:10:46 +0000

    Update profile.php

--- jjig/profile.php
+++ jjig/profile.php
@@ -118,16 +118,26 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+btn.onclick = function() {
+  modal2.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+span.onclick = function() {
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal2.style.display = "none";
   }
 }
 </script>
